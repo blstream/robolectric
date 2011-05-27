97e21685ee21389a00eb6d9382aab47ae06b77c3
Marcin Jancewicz <marcin.jancewicz@blstream.com>
2011-05-27 15:45:53 +0200

    getDir

--- src/main/java/com/xtremelabs/robolectric/shadows/ShadowContext.java
+++ src/main/java/com/xtremelabs/robolectric/shadows/ShadowContext.java
@@ -17,81 +17,84 @@ import static com.xtremelabs.robolectric.Robolectric.shadowOf;
 /**
  * Calls through to the {@code resourceLoader} to actually load resources.
  */
-@SuppressWarnings({"UnusedDeclaration"})
+@SuppressWarnings({ "UnusedDeclaration" })
 @Implements(Context.class)
 abstract public class ShadowContext {
-    @RealObject private Context realContext;
+	@RealObject
+	private Context realContext;
 
-    @Implementation
-    public String getString(int resId) {
-        return realContext.getResources().getString(resId);
-    }
+	@Implementation
+	public String getString(int resId) {
+		return realContext.getResources().getString(resId);
+	}
 
-    @Implementation
-    public CharSequence getText(int resId) {
-        return realContext.getResources().getText(resId);
-    }
+	@Implementation
+	public CharSequence getText(int resId) {
+		return realContext.getResources().getText(resId);
+	}
 
-    @Implementation
-    public String getString(int resId, Object... formatArgs) {
-        return realContext.getResources().getString(resId, formatArgs);
-    }
+	@Implementation
+	public String getString(int resId, Object... formatArgs) {
+		return realContext.getResources().getString(resId, formatArgs);
+	}
 
-    @Implementation
-    abstract public Resources.Theme getTheme();
+	@Implementation
+	abstract public Resources.Theme getTheme();
 
-    @Implementation
-    public final TypedArray obtainStyledAttributes(
-            int[] attrs) {
-        return getTheme().obtainStyledAttributes(attrs);
-    }
+	@Implementation
+	public final TypedArray obtainStyledAttributes(int[] attrs) {
+		return getTheme().obtainStyledAttributes(attrs);
+	}
 
-    @Implementation
-    public final TypedArray obtainStyledAttributes(
-            int resid, int[] attrs) throws Resources.NotFoundException {
-        return getTheme().obtainStyledAttributes(resid, attrs);
-    }
+	@Implementation
+	public final TypedArray obtainStyledAttributes(int resid, int[] attrs) throws Resources.NotFoundException {
+		return getTheme().obtainStyledAttributes(resid, attrs);
+	}
 
-    @Implementation
-    public final TypedArray obtainStyledAttributes(
-            AttributeSet set, int[] attrs) {
-        return getTheme().obtainStyledAttributes(set, attrs, 0, 0);
-    }
+	@Implementation
+	public final TypedArray obtainStyledAttributes(AttributeSet set, int[] attrs) {
+		return getTheme().obtainStyledAttributes(set, attrs, 0, 0);
+	}
 
-    @Implementation
-    public final TypedArray obtainStyledAttributes(
-            AttributeSet set, int[] attrs, int defStyleAttr, int defStyleRes) {
-        return getTheme().obtainStyledAttributes(
-            set, attrs, defStyleAttr, defStyleRes);
-    }
+	@Implementation
+	public final TypedArray obtainStyledAttributes(AttributeSet set, int[] attrs, int defStyleAttr, int defStyleRes) {
+		return getTheme().obtainStyledAttributes(set, attrs, defStyleAttr, defStyleRes);
+	}
 
-    @Implementation
-    public File getCacheDir() {
-        // todo: clean this up when tests finish? [xw 20110124]
-        File file = new File(System.getProperty("java.io.tmpdir"), "android-cache");
-        file.mkdirs();
-        return file;
-    }
+	@Implementation
+	public File getCacheDir() {
+		// todo: clean this up when tests finish? [xw 20110124]
+		File file = new File(System.getProperty("java.io.tmpdir"), "android-cache");
+		file.mkdirs();
+		return file;
+	}
 
-    @Implementation
-    public File getFilesDir() {
-        // todo: clean this up when tests finish? [xw 20110124]
-        File file = new File(System.getProperty("java.io.tmpdir"), "android-tmp");
-        file.mkdirs();
-        return file;
-    }
+	@Implementation
+	public File getFilesDir() {
+		// todo: clean this up when tests finish? [xw 20110124]
+		File file = new File(System.getProperty("java.io.tmpdir"), "android-tmp");
+		file.mkdirs();
+		return file;
+	}
 
-    @Implementation
-    public File getFileStreamPath(String name) {
-        return new File(getFilesDir(), name);
-    }
+	@Implementation
+	public File getFileStreamPath(String name) {
+		return new File(getFilesDir(), name);
+	}
 
-    /**
-     * Non-Android accessor.
-     *
-     * @return the {@code ResourceLoader} associated with this {@code Context}
-     */
-    public ResourceLoader getResourceLoader() {
-        return shadowOf((Application) realContext.getApplicationContext()).getResourceLoader();
-    }
+	@Implementation
+	public File getDir(java.lang.String name, int mode) {
+		File file = new File(System.getProperty("java.io.tmpdir"), "android-database");
+		file.mkdirs();
+		return file;
+	}
+
+	/**
+	 * Non-Android accessor.
+	 * 
+	 * @return the {@code ResourceLoader} associated with this {@code Context}
+	 */
+	public ResourceLoader getResourceLoader() {
+		return shadowOf((Application) realContext.getApplicationContext()).getResourceLoader();
+	}
 }
